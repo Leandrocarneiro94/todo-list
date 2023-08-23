b721a2c8bf88fa2ef389eb6fb1c6912e0c2fb27b
leandrofisio94@gmail.com <leandrofisio94@gmail.com>
2023-08-23 12:15:50 -0300

    Add JS att

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 const button = document.querySelector('[data-add-button]')
 const list = document.querySelector('[data-list]')
+const categoriesList = document.querySelector('[data-categories]')
+const buttonAddCategory = document.querySelector('[data-add-category]')
+const categoryTitle = document.querySelector('[data-category-title]')
+
+const categories = [
+    // {
+    //     id: 1,
+    //     category:"Tarefas de Casa",
+    //     items: [
+    //         {
+    //             id: 1,                
+    //             item: "",
+    //             checked: true,
+    //         }
+    //     ],
+    // },
+]
 
 const createItemList = (id) => {
     const item = document.createElement('li')
@@ -10,14 +27,14 @@ const createItemList = (id) => {
     `
 
     const label = document.createElement('label')
+    label.setAttribute('for', `item-${id}`)
+    label.classList.add('card')
     label.innerHTML = `
         <div class="checkField">
             <img class="image" src="./Icons/checkmark.png"/>
         </div>
         <input class="checkbox__text" />
     `
-    label.setAttribute('for', `item-${id}`)
-    label.classList.add('card')
 
     const deleteButton = document.createElement('button')
     deleteButton.classList.add('deleteButton')
@@ -36,10 +53,67 @@ const createItemList = (id) => {
 button.addEventListener('click', () => {
     const newId = list.childElementCount + 1
     const element = createItemList(newId)
-    list.append(element)
+    list.prepend(element)
 
     const inputField = element.querySelector('.checkbox__text');
     inputField.focus();
 })
 
-// _____________________________divisoria________________________________________
+// -------------------------------------------------------------------------------------------------------
+
+const handleActiveCategory = (categoryElement, currentCategory) => {
+    categoriesList.childNodes.forEach((categoryNode) => {
+        if(categoryNode.classList) {
+            categoryNode.querySelector('.navBar__category').classList.remove('navBar__category--active')
+            categoryTitle.innerHTML = currentCategory.category
+        }
+    })
+    categoryElement.querySelector('.navBar__category').classList.add('navBar__category--active')
+}
+
+const createCategoryElement = (element) => {
+    const categoryElement = document.createElement('li')
+    const currentCategory = categories.find((category) => category.id === element.id)
+
+    const categoryInput = document.createElement('input')
+    categoryInput.classList.add('navInput')
+    categoryInput.addEventListener('keyup', (e) => {
+        currentCategory.category = e.target.value
+    })
+    categoryElement.appendChild(categoryInput)
+    categoryElement.addEventListener('click', () => handleActiveCategory(categoryElement, currentCategory))
+    
+    const categoryButton = document.createElement('button')
+    categoryButton.innerHTML = `
+        <span class="navBar__category__counter">1/4</span>
+    `
+    categoriesList.childNodes.forEach((categoryNode) => {
+        if(categoryNode.classList) {
+            categoryNode.querySelector('.navBar__category').classList.remove('navBar__category--active')
+            categoryTitle.innerHTML = currentCategory.category
+        }
+    })
+    
+    categoryButton.classList.add('navBar__category')
+    categoryButton.classList.add('navBar__category--active')
+    categoryButton.prepend(categoryInput)
+    categoryElement.appendChild(categoryButton)
+
+    return categoryElement
+}
+
+const handleAddCategory = () => {
+    const newId = categoriesList.childElementCount + 1
+    const newCategory = {        
+        id: newId,
+        category: '',
+        items: []
+    }
+    categories.push(newCategory)
+    const  categoryElement = createCategoryElement(newCategory)
+    categoriesList.appendChild(categoryElement)
+    console.log(newCategory)
+}
+
+buttonAddCategory.addEventListener('click', handleAddCategory)
+
